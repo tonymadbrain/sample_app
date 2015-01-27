53e57b305b3a210a9e3274a88188a96e77614478
tonymadbrain <mb7222@yandex.ru>
2015-01-27 12:30:16 +0300

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,6 +24,13 @@ module SessionsHelper
       user == current_user
     end
 
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
   	def sign_out
   		current_user.update_attribute(:remember_token,
   									  User.encrypt(User.new_remember_token))
@@ -39,5 +46,7 @@ module SessionsHelper
     def store_location
       session[:return_to] = request.url if request.get?
     end
+
+
 
 end
